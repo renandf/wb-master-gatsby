1aedb5cf889ec4f3461b97ae700026d1e4fd80c6
Renan Castro <renandf@gmail.com>
2020-12-28 22:22:24 +1100

    feat: Create loading state for images
    Create an animated placeholder for images while loading

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import useLatestData from '../utils/useLatestData';
+import LoadingGrid from '../components/LoadingGrid';
+import { HomePageGrid } from '../styles/Grids';
 
-function CurrentSlicing() {
+function CurrentSlicing({ slicemasters }) {
   return (
     <div>
-      <p>CurrentSlicing</p>
+      {!slicemasters && <LoadingGrid count={4} />}
+      {slicemasters && !slicemasters?.length && <p>No one is working</p>}
     </div>
   );
 }
-function HotSlices() {
+function HotSlices({ hotSlices }) {
   return (
     <div>
-      <p>HotSlices</p>
+      {!hotSlices && <LoadingGrid count={4} />}
+      {hotSlices && !hotSlices?.length && <p>Nothing in the case</p>}
     </div>
   );
 }
@@ -23,10 +27,10 @@ export default function HomePage() {
     <div className="center">
       <h1>The Best Pizza Downtown!</h1>
       <p>Open 11am to 11pm every single day</p>
-      <div>
+      <HomePageGrid>
         <CurrentSlicing slicemasters={slicemasters} />
         <HotSlices hotSlices={hotSlices} />
-      </div>
+      </HomePageGrid>
     </div>
   );
 }
