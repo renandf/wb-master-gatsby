f85d2aa5e041f2f3f051a8752172c0256bb38879
Renan Castro <renandf@gmail.com>
2020-12-28 16:15:36 +1100

    feat: Create custom hook for client side data fetching
    Create useLatestData hook to fetch slicemasters and pizza options from client side

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import useLatestData from '../utils/useLatestData';
+
+function CurrentSlicing() {
+  return (
+    <div>
+      <p>CurrentSlicing</p>
+    </div>
+  );
+}
+function HotSlices() {
+  return (
+    <div>
+      <p>HotSlices</p>
+    </div>
+  );
+}
 
 export default function HomePage() {
+  const { slicemasters, hotSlices } = useLatestData();
+
   return (
-    <>
-      <p>Hey, I'm the home page</p>
-    </>
+    <div className="center">
+      <h1>The Best Pizza Downtown!</h1>
+      <p>Open 11am to 11pm every single day</p>
+      <div>
+        <CurrentSlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </div>
+    </div>
   );
 }
